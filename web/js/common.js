715428d15bb12fddaa3b34e086d387a76bf8ec7c
liliukuan <792362317@qq.com>
2018-07-24 20:54:03 +0800

    粒子图

--- web/js/common.js
+++ web/js/common.js
@@ -4,43 +4,19 @@ function mouseWheel(){
 	    if (dir == 'Up') {
 	        if($(window).scrollTop() < 260){
 				$("header").css("top","30px");
-	        	$("header ul.nav > li > a").css("color","#666");
-	        	$("header .container .navbar").css("background","white");
 	        	$("header .container").removeClass("commonWidth");
 	        }
 	    } else {
 	        if($(window).scrollTop() > 160){
 				$("header").css("top","0");
-				$("header .container").addClass("commonWidth");
-	        	$("header .container .navbar").css("background","white");
-	        	
+				$("header .container").addClass("commonWidth");     	
 	        }
 	    }		
 	});
 }
-//mouseWheel();
-console.log( /Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent));
 
 if(/Android|webOS|iPhone|iPod/i.test(navigator.userAgent)){
-	
-//	$('body').bind('touchmove', function(e) { 
-//	    e.preventdefault();               //禁用默认滚动行为，需要自己实现滚动
-//	    alert($(this).scrollTop()); // 计算你的屏幕高度
-//	    if($(this).scrollTop() < 260){
-//			$("header").css("top","30px");
-//      	$("header ul.nav > li > a").css("color","#666");
-//      	$("header .container .navbar").css("background","white");
-//      	$("header .container").removeClass("commonWidth");
-//      }else{
-//      	$("header").css("top","0");
-//			$("header .container").addClass("commonWidth");
-//      	$("header .container .navbar").css("background","white");
-//      }
-//	});
-	window.addEventListener('onscroll', function() {
-	    var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-	    alert(scrollTop);
-	})
+	$("header .container").addClass("mobileSty");
 }else{
 	mouseWheel();
 }
