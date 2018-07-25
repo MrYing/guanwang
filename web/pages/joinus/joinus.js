abe87cb70a4c733adaba45c4910fef2d58736366
liliukuan <792362317@qq.com>
2018-07-25 17:15:07 +0800

    样式的更改，

--- web/pages/joinus/joinus.js
+++ web/pages/joinus/joinus.js
@@ -3,31 +3,30 @@ $("#accordion>.panel>.panel-heading").each(function(k,v){
 	var that = $(this);
 	
 	$(this).click(function(){
-//		if($(this))
-		$(this).parent().siblings(".panel").children(".panel-heading").removeClass("bg")
-		
-		console.log($(this).parent(".panel").siblings().children(""))
-		$(this).addClass("bg");
-//		console.log($(this).find("img").attr("src"))
-		if($(this).find("img").attr("src") == "img/收起.png"){
-			$(this).find("img").attr("src","img/展开.png")
+
+		if($(this).siblings().hasClass("in")){
+			$(this).find("img").attr("src","img/展开.png");
+			$(this).parent(".panel").siblings().children(".panel-heading").find("img").attr("src","img/展开.png");
+			$(this).removeClass("bg");
+			$(this).find("span").css("color","#666");
+			$(this).parent(".panel").siblings().children(".panel-heading").find("span").css("color","#666");
+			
 		}else{
-			$(this).find("img").attr("src","img/收起.png")
-		}
-		
-
-	
+			$(this).find("span").css("color","#fff");
+			$(this).addClass("bg");
+			$(this).parent(".panel").siblings().children(".panel-heading").find("span").css("color","#666");
+			$(this).parent().siblings(".panel").children(".panel-heading").removeClass("bg")	
+			$(this).find("img").attr("src","img/收起.png");
+			$(this).parent(".panel").siblings().children(".panel-heading").find("img").attr("src","img/展开.png");		
+		}	
 		var id = $(this).find("a").attr("href");
-		$(id).collapse('toggle');
-		console.log(222)
-
-	})
+		$(id).collapse('toggle');		
+		$(this).parent(".panel").siblings().children(".collapse").removeClass("in");	
+	})	
 })
 
 
-	$(".apply").click(function(){
-		console.log(222)
-		alert("请以具体的招聘职位名称（如：Java开发工程师）为主题，将简历投递至 rose@ofaai.com邮箱中！")
-		
-//		alert('<p style="font-size: 16px;color: #FF5F5F;margin-left: 40px;"></p>')
-	})
+//$(".apply").click(function(){
+//	console.log(222)
+//	alert("请以具体的招聘职位名称（如：Java开发工程师）为主题，将简历投递至 rose@ofaai.com邮箱中！")
+//})
